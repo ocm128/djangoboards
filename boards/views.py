f5c77d867bc303dc856db9e24b57c5bda955fe43
kalasni <kalassni@gmail.com>
2018-04-15 20:34:24 +0200

    Added new unit test and created view and template topics.html and base.html. Modified templates to use base.html

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+# from django.http import Http404
 # from django.http import HttpResponse
 
 # from boards import views
@@ -15,5 +16,9 @@ def home(request):
 
     response_html = '<br>'.join(boards_name)
     """
-
     return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
