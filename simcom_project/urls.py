a715d3a07269810c630d33d4e9f03caae3ab03e3
kalasni <kalassni@gmail.com>
2018-04-26 21:15:57 +0200

    Added templates and views login, logout and styling with jquery and popper.js

--- simcom_project/urls.py
+++ simcom_project/urls.py
@@ -23,6 +23,9 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'),
+         name='login'),
+
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
