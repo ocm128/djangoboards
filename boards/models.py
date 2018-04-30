6726d98fed0241e6c4fc7013961eaf485269744c
kalasni <kalassni@gmail.com>
2018-04-30 20:53:44 +0200

    Created reply_topic view and inplemented display posts, topics count and last user who posted

--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
 
 
 class Board(models.Model):
@@ -8,6 +9,13 @@ class Board(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(
+            topic__board=self).order_by('-created_at').first()
 
 
 class Topic(models.Model):
@@ -28,6 +36,9 @@ class Topic(models.Model):
     starter = models.ForeignKey(User, null=True, related_name='topics',
                                 on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.subject
+
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -42,3 +53,7 @@ class Post(models.Model):
     # relationship, so it will ignore it.
     updated_by = models.ForeignKey(User, null=True, related_name='+',
                                    on_delete=models.SET_NULL)
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
--- boards/views.py
+++ boards/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import User
+from django.db.models import Count
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 # from django.http import Http404
 # from django.http import HttpResponse
 
-from .forms import NewTopicForm
+from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
 
 
@@ -23,7 +25,9 @@ def home(request):
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_update').annotate(
+        replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 
 @login_required
@@ -46,9 +50,9 @@ def new_topic(request, pk):
                 created_by=request.user
             )
             # redirect to the created topic page
-            return redirect('board_topics', pk=board.pk)
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
-            # The request is GET
+        # The request is GET
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
@@ -56,3 +60,20 @@ def new_topic(request, pk):
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     return render(request, 'topic_posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
